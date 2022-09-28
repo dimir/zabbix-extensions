43b35c439b6740346c0f9c9b67ef5c3d0aa0aaf9
dimir <vladimir.levijev@zabbix.com>
2022-09-28 18:51:42 +0300

    add frontend rebranding example

---
+++ frontend/rebranding/brand.conf.php
@@ -0,0 +1,13 @@
+<?php
+
+# removes the "support" and "integration" menu elements
+# return [];
+
+# in addition rebrands 5 elements
+return [
+    'BRAND_LOGO'                 => 'https://github.com/dimir/zabbix-extensions/tree/main/frontend/rebranding/images/lorem-ipsum.png',
+	'BRAND_LOGO_SIDEBAR'         => 'https://github.com/dimir/zabbix-extensions/tree/main/frontend/rebranding/images/lorem.png',
+	'BRAND_LOGO_SIDEBAR_COMPACT' => 'https://github.com/dimir/zabbix-extensions/tree/main/frontend/rebranding/images//l.png',
+	'BRAND_FOOTER'               => '(c) Lorem Ipsum',
+	'BRAND_HELP_URL'             => 'https://www.example.com/help/'
+];
